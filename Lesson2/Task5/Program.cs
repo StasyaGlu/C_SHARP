0a39dcf76787f1d9a89eed29800a6b78a1e660f2
Anastasiya Glushakova <stasyalife2312@gmail.com>
2024-07-08 21:44:55 +0300

    add Task5

---
+++ Lesson2/Task5/Program.cs
@@ -0,0 +1,38 @@
+﻿// Условие задачи 4
+
+int n = 5;
+int[] arr = { 2, 4, 7, 3, 9};
+int i = 0;
+int max = arr[0];
+
+while (i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i++;
+}
+System.Console.WriteLine(max);
+
+max = arr[0];
+for (int j = 0; j < n; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+
+}
+System.Console.WriteLine(max);
+
+
+max = arr[0];
+foreach (int e in arr)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
