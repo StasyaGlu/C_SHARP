1d795e1c7f6e9fd809109620bc05f3377581aabf
Anastasiya Glushakova <stasyalife2312@gmail.com>
2024-07-08 21:33:11 +0300

    add Lesson3:Task1

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычислить значение формулы (a*b) / (c+d) (это дробь)
+// где a, b, c, d - некоторые целые числа. Решение оформить 
+// в виде функции.
+
+double ColculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenetor = c + d;
+    double resalt = numenator / denumenetor;
+    return resalt;
+}
+double resalt = ColculateFormula(1, 2, 3, 4);
+System.Console.WriteLine(resalt);
+ 
