429e8f5a7dec4a80d36bd2c9def9397e0ab3ffa7
Anastasiya Glushakova <stasyalife2312@gmail.com>
2024-07-08 21:43:20 +0300

    add Task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано 5 гирь с разным весом в случайном
+// порядке. Определить вес самой тяжёлой
+// гири.
+
+int n = 5;
+int[] arr = { 2, 4, 7, 3, 9};
+int i = 0;
+int max = arr[0];
+
+while (i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i++;
+}
+System.Console.WriteLine(max);
